d86d5b2c12053a4493ebeb7b379a061a15dd5829
mohrmannhalsey5 <mohrmannhalsey5@gmail.com>
2025-03-07 09:05:21 +0000

    Add 2.ts

---
+++ src/2.ts
@@ -0,0 +1,6 @@
+// Generate a random number between 1 and 10
+function getRandomNumber() {
+    return Math.floor(Math.random() * 10) + 1;
+}
+
+console.log("Your random number is: " + getRandomNumber());
